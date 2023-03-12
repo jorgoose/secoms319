8da1d903b69f635576ff4a3369d490e52487727b
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-03-11 23:43:57 -0600

    Local API request first iteration.

--- midterm-project/api/main.py
+++ midterm-project/api/main.py
@@ -3,6 +3,15 @@ import requests
 
 app = FastAPI()
 
+# Allow all origins to access the API
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -17,10 +26,10 @@ async def search(query: str):
     return response.json()
 
 @app.get("/plant/{plant_id}")
-async def get_plant(plant_id: int):
+async def get_plant(plant_id: str):
 
     # Make request to API and return plant data based on plant_id
-    url = "http://trefle.io/api/v1/species/" + plant_id + "?token=TI9-m33c5nRHwoDguCODeSS_dsOacOs_J81N9MHdQzg"
+    url = "http://trefle.io/api/v1/species/" + str(plant_id) + "?token=TI9-m33c5nRHwoDguCODeSS_dsOacOs_J81N9MHdQzg"
 
     response = requests.get(url)
     return response.json()

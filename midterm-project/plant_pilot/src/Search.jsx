34583ac823fe0c3b55346e295c9209f150231fc4
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-03-11 22:12:45 -0600

    Added basics for fetch and path variable for search result page.

--- midterm-project/plant_pilot/src/Search.jsx
+++ midterm-project/plant_pilot/src/Search.jsx
@@ -2,11 +2,33 @@
 
 import React from "react";
 
+// Access URL path variable
+import { useParams } from "react-router-dom";
+
+// Access state variables
+import { useState, useEffect } from "react";
+
 function Search() {
+  const { query } = useParams();
+
+  //   Variable to store search results
+  const [results, setResults] = useState([]);
+
+  //   TODO - fetch data from API based on query and save results on page load
+  useEffect(() => {
+    // fetch("http://localhost:3001/search/" + query)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setResults(data);
+    //   });
+  }, []);
+
   return (
     <div>
       <div className="col-span-4 m-auto mt-8">
-        <h1 className="text-4xl font-bold text-center">Search Results</h1>
+        <h1 className="text-4xl font-bold text-center">
+          Search Results for "{query}"
+        </h1>
       </div>
       <div className="m-auto ml-8 mb-8">
         <img

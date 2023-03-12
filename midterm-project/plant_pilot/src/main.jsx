0458695e727fcf3caa9b92f00678861789bba5ad
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-03-11 21:45:19 -0600

    Added plant route

--- midterm-project/plant_pilot/src/main.jsx
+++ midterm-project/plant_pilot/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
+import Plant from "./Plant";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+
+    path: "/plant/:id",
+    element: <Plant />,
   },
 ]);
 

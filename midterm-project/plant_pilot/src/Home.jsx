0f7d65c873f0d0f0e029219972fa59bd1a355617
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-03-12 21:55:45 -0500

    Updated home with some plants.

--- midterm-project/plant_pilot/src/Home.jsx
+++ midterm-project/plant_pilot/src/Home.jsx
@@ -7,18 +7,45 @@ import Navbar from "./components/Navbar/Navbar";
 import Humidity from "./components/Humidity";
 
 function Home() {
-  const [temp, setTemp] = useState(0);
+  const [temp, setTemp] = useState(60);
+
+  const [loading, setLoading] = useState(true);
+
+  // Array of plants to display, total of 8 plants
+  let [plants, setPlants] = useState([
+    {
+      id: 249477,
+      data: {},
+    },
+    {
+      id: 236068,
+      data: {},
+    },
+    {
+      id: 110975,
+      data: {},
+    },
+    {
+      id: 423390,
+      data: {},
+    },
+  ]);
 
   // Function to set current temperature at load of page
   useEffect(() => {
-    // fetch("http://localhost:3001/temp")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setTemp(data);
-    //   });
+    // Loop through plants and fetch data from API
+    plants.forEach((plant) => {
+      // Get method to "https://localhost:8000/plant/" + id
+      fetch("http://localhost:8000/plant/" + plant.id)
+        .then((response) => response.json())
+        .then((data) => {
+          // Add the plant data to the plant object
+          plant.data = data.data;
+        });
+    });
 
-    // Placeholder
-    setTemp(60);
+    // Loading is done
+    setLoading(false);
   }, []);
 
   return (
@@ -32,6 +59,12 @@ function Home() {
             type="text"
             placeholder="Search for a Plant Here:"
             className="border px-2 py-2 rounded w-80"
+            // When user presses enter, redirect to search page
+            onKeyUp={(event) => {
+              if (event.key === "Enter") {
+                window.location.href = "/search/" + event.target.value;
+              }
+            }}
           ></input>
         </div>
         <div className="..."></div>
@@ -46,9 +79,6 @@ function Home() {
             height="300"
           />
           <h1 className="mt-8">Current Temp: {temp}°C</h1>
-          <h1 className="mt-2">
-            Optimal Temp for &#123;Plant Name&#125;: {temp}°C
-          </h1>
         </div>
         <div className="...">
           <Humidity />
@@ -57,94 +87,27 @@ function Home() {
         <div className="col-span-4 m-auto my-8 font-bold">
           A Bunch of Examples of Common Plants Will be Shown Below Here:
         </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 1</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 2</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 3</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 4</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 5</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 6</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 7</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
-        <div className="m-auto ml-8 mb-8">
-          <img
-            className="rounded-lg shadow-md mb-3"
-            src="https://tailwindcss.com/img/card-top.jpg"
-          />
-          <h2 className="font-semibold text-center">Plant 8</h2>
-          <p>
-            Plant description. Here will be more details about the specific
-            plant being shown...
-          </p>
-        </div>
+        {/* For each plant in plants, create a set of elements */}
+        {plants.map((plant) => (
+          <div className="m-auto ml-8 mb-8">
+            {/* Image wrapped in a div to crop it to a consistent square size, using object-fit */}
+            <div className="w-72 h-72 overflow-hidden rounded-lg shadow-md m-auto mb-3">
+              <img
+                src={loading ? reactLogo : plant.data.image_url}
+                alt="Plant"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <h2 className="font-semibold text-center">
+              {plant.data.common_name}
+            </h2>
+            <h3 className="text-center">{plant.data.scientific_name}</h3>
+            <p>
+              Plant description. Here will be more details about the specific
+              plant being shown...
+            </p>
+          </div>
+        ))}
       </div>
       <div className="text-center pb-6">...</div>
     </div>

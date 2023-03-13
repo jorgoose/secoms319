020bf677a7f5dcb0c6aed21cff22da4fc6a4df00
djs1 <djs1@iastate.edu>
2023-03-12 22:05:40 -0500

    added padding

--- midterm-project/plant_pilot/src/components/Navbar/Navbar.jsx
+++ midterm-project/plant_pilot/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <div class="container flex flex-wrap items-center justify-between px-20 py-5">
         <a className="logo" href="http://localhost:5173">PlantPilot</a>
         <a className="logo" href="http://localhost:5173/credits">Credits</a>
       </div>

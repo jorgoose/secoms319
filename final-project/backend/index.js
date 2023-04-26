054dbe2ab3eeaca874bf28838eae3396e960ba28
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-04-26 16:31:57 -0500

    Version 1 for final project.

--- final-project/backend/index.js
+++ final-project/backend/index.js
@@ -1,9 +1,41 @@
 // Basic ExpressJS backend
-
 const express = require("express");
+
+// CORS
+const cors = require("cors");
+
+// Axios is a library for making HTTP requests
+const axios = require("axios");
 
 const app = express();
 
+// Enable CORS
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+// Define the endpoint to retrieve the featured games
+app.get("/api/featured", async (req, res) => {
+  try {
+    // Make a request to the Steam Web API to get the featured games data
+    const response = await axios.get(
+      "https://store.steampowered.com/api/featured/?cc=us&l=english&json=1&cc=us&l=english&v=1&format=json"
+    );
+
+    // Extract the featured_win array from the response and assing to the "result" variable
+    const result = response.data.featured_win;
+
+    // Send the result back to the client
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve featured games data" });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
--- final-project/frontend/src/App.tsx
+++ final-project/frontend/src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const [games, setGames] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/featured")
+      .then((response) => setGames(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredGames = games.filter((game) =>
+    game.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col h-screen">
+      {/* Header */}
+      <header className="bg-gray-900 text-white py-4 px-8 flex justify-between items-center">
+        <h1 className="font-bold text-lg">Featured Games</h1>
+        <div className="flex items-center space-x-4">
+          <input
+            type="text"
+            placeholder="Search Games"
+            className="py-2 px-4 rounded-lg bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button className="focus:outline-none">
+            <a href="#" className="relative">
+              <img
+                src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG4.png"
+                alt="cart"
+                className="w-12 h-12"
+              />
+              <span className="absolute top-0 right-0 bg-red-600 text-white rounded-full w-6 h-6 text-xs flex items-center justify-center">
+                0
+              </span>
+            </a>
+          </button>
+        </div>
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-1 px-8 py-4 overflow-y-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {filteredGames.map((game) => (
+            <div key={game.id} className="bg-white rounded-lg shadow-lg">
+              <img
+                src={game.large_capsule_image}
+                alt={game.name}
+                className="rounded-t-lg"
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-bold">{game.name}</h2>
+                <p className="text-sm text-gray-600">
+                  {game.currency} {game.final_price / 100}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-900 text-gray-500 py-4 px-8 text-center">
+        <p>&copy; 2022 Steam Store</p>
+      </footer>
+    </div>
   );
-}
+};
 
 export default App;
